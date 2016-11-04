3581694886c45c0868d7d8c38305af9f4ff8f669
MentalyDifereny <mailandrewp@gmail.com>
2016-11-03 20:03:26 -0500

    First Commit

---
+++ Assets/Enter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Invector;
+
+public class Enter : MonoBehaviour {
+
+	void OnStart (){
+	//	GameObject.Find("CarCamera").SetActive(false);
+	//	GameObject.Find ("CarCamera").GetComponent<v3rdPersonCamera> ().enabled = false;
+	}
+	void OnTriggerEnter (Collider col) {
+		Debug.Log ("Player In Trigger");
+		if (col.tag == "Player") {
+			GameObject.Find("Camera").SetActive(false);
+			GameObject.Find("CarCamera").SetActive(true);
+			//GameObject.Find ("CarCamera").GetComponent<v3rdPersonCamera> ().enabled = true;
+			//MonoBehaviour cont = col.GetComponent<vThirdPersonController>();
+			//cont.enabled = false;
+			col.enabled=false;
+			GameObject.Find ("CXmazdaside(2)").GetComponent<CharacterController> ().enabled = true;
+		}
+
+	}
+}
